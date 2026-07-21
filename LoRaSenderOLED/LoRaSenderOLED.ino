7f0a1aa3755605424ed889646c03529bd1b42043
AlexandreFrolov <alexandre@frolov.pp.ru>
2026-07-21 08:43:55 +0300

    Add OLED

--- LoRaSenderOLED/LoRaSenderOLED.ino
+++ LoRaSenderOLED/LoRaSenderOLED.ino
@@ -1,4 +1,4 @@
-/* Heltec Automation send communication test example
+/* Heltec Automation send communication test example + OLED output
  *
  * Function:
  * 1. Send data from a esp32 device over hardware 
@@ -16,6 +16,11 @@
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
 
+// OLED (I2C, встроенный дисплей Heltec)
+#include <Wire.h>
+#include "HT_SSD1306Wire.h"
+
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
 
 /*#define RF_FREQUENCY                                915000000 // Hz */
 #define RF_FREQUENCY                                868900000 // Hz
@@ -47,14 +52,72 @@ double txNumber;
 
 bool lora_idle=true;
 
+uint32_t txCount = 0;
+bool lastTxOk = true; // true = TX done, false = TX timeout
+
 static RadioEvents_t RadioEvents;
 void OnTxDone( void );
 void OnTxTimeout( void );
 
+// Включение питания OLED (Vext) на платах Heltec
+void VextON(void)
+{
+  pinMode(Vext, OUTPUT);
+  digitalWrite(Vext, LOW);
+}
+
+void VextOFF(void)
+{
+  pinMode(Vext, OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
+
+// Экран с только что отправленным пакетом (пока идёт передача)
+void drawSendingScreen()
+{
+  display.clear();
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "LoRa TX #" + String(txCount));
+
+  display.setFont(ArialMT_Plain_16);
+  display.drawStringMaxWidth(0, 14, 128, String(txpacket));
+
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 54, "Sending...");
+
+  display.display();
+}
+
+// Экран с итогом передачи (успех / таймаут)
+void drawStatusScreen()
+{
+  display.clear();
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "LoRa TX #" + String(txCount));
+
+  display.setFont(ArialMT_Plain_16);
+  display.drawStringMaxWidth(0, 14, 128, String(txpacket));
+
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 54, lastTxOk ? "TX done" : "TX timeout");
+
+  display.display();
+}
+
 void setup() {
     Serial.begin(115200);
     Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
-	
+
+    // Инициализация OLED
+    VextON();
+    delay(100);
+    display.init();
+    display.setFont(ArialMT_Plain_10);
+
     txNumber=0;
 
     RadioEvents.TxDone = OnTxDone;
@@ -66,6 +129,13 @@ void setup() {
                                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
                                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
                                    true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
+
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "LoRa Sender");
+    display.drawString(0, 20, "Ready to send...");
+    display.display();
    }
 
 
@@ -80,6 +150,9 @@ void loop()
    
 		Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
 
+		txCount++;
+		drawSendingScreen();
+
 		Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out	
     lora_idle = false;
 	}
@@ -89,6 +162,8 @@ void loop()
 void OnTxDone( void )
 {
 	Serial.println("TX done......");
+	lastTxOk = true;
+	drawStatusScreen();
 	lora_idle = true;
 }
 
@@ -96,5 +171,7 @@ void OnTxTimeout( void )
 {
     Radio.Sleep( );
     Serial.println("TX Timeout......");
+    lastTxOk = false;
+    drawStatusScreen();
     lora_idle = true;
 }
